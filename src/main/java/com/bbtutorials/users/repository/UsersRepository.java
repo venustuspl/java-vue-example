fd8eda16d3595f861d0bb53eae7183652222431b
venustuspl <venustus.pl@gmail.com>
2021-02-26 22:04:19 +0100

    firt commit

--- src/main/java/com/bbtutorials/users/repository/UsersRepository.java
+++ src/main/java/com/bbtutorials/users/repository/UsersRepository.java
@@ -1,11 +1,12 @@
 package com.bbtutorials.users.repository;
 
+import com.bbtutorials.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.bbtutorials.users.entity.Users;
-
 @RepositoryRestResource()
-public interface UsersRepository extends JpaRepository<Users, Integer>, JpaSpecificationExecutor<Users>, QuerydslPredicateExecutor<Users> {}
+public interface UsersRepository extends JpaRepository<Users, Integer>,
+        JpaSpecificationExecutor<Users>, QuerydslPredicateExecutor<Users> {
+}
