5414eccc9c31fe5b74940f9d9b8b88445c2e8e46
venustuspl <venustus.pl@gmail.com>
2021-03-11 21:08:02 +0100

    fix: template view problems

--- src/main/ui/src/components/Dashboard.vue
+++ src/main/ui/src/components/Dashboard.vue
@@ -1,48 +1,28 @@
-<
-template >
-    <
-    div class = "hello" >
-    <
-    Header / >
-    <
-    div class = "container mrgnbtm" >
-    <
-    div class = "row" >
-    <
-    div class = "col-md-8" >
-    <
-    FormComponent @createUser = "userCreate($event)" / >
-    <
-    /div> <
-    div class = "col-md-4" >
-    <
-    DisplayBoard: numberOfUsers = "numberOfUsers"
-@getAllUsers = "getAllUsers()" / >
-    <
-    /div> <
-    div class = "text" > {
-        {
-            saveErrors
-        }
-    } <
-    /div> <
-    div class = "mx-auto"
-style = "width: 100%;" >
-    <
-    Users v -
-    if = "users.length > 0": users = "users" / >
-    <
-    /div> <
-    /div> <
-    /div>
+<template>
+  <div class="hello">
+    <Header />
+    <div class="container mrgnbtm">
+          <div class="row">
+            <div class="col-md-8">
+              <FormComponent @createUser="userCreate($event)" />
+            </div>
+            <div class="col-md-4">
+                <DisplayBoard :numberOfUsers="numberOfUsers" @getAllUsers="getAllUsers()" />
+            </div>
+           <div class="text">
+           {{saveErrors}}
+           </div>
+                    <div class="mx-auto" style="width: 100%;">
+                        <Users v-if="users.length > 0" :users="users" />
+                    </div>
+          </div>
+    </div>
 
-    <
-    /div> <
-    /template>
+  </div>
+</template>
 
-    <
-    script >
-    import Header from './Header.vue'
+        <script>
+import Header from './Header.vue'
 import DisplayBoard from './DisplayBoard.vue'
 import Users from './Users.vue'
 import FormComponent from './FormComponent.vue'
@@ -71,22 +51,22 @@ export default {
     methods: {
         getAllUsers() {
             getAllUsers().then(response => {
-                this.users = response
-                this.numberOfUsers = this.users.length
+                this.users=response
+                this.numberOfUsers=this.users.length
             })
         },
         userCreate(data) {
-            this.saveErrors = "";
-            data.id = this.numberOfUsers + 1
+            this.saveErrors="";
+            data.id=this.numberOfUsers + 1
             createUser(data).then((response) => {
                 console.log("***");
                 response.text().then((text) => {
                     console.log(text);
-                    this.saveErrors = text;
+                    this.saveErrors=text;
                 });
                 console.log("00");
                 if (response.status == 403) {
-                    this.saveErrors = response.body;
+                    this.saveErrors=response.body;
                 }
                 this.getAllUsers();
             });
@@ -95,5 +75,4 @@ export default {
     mounted() {
         this.getAllUsers();
     }
-} <
-/script>
+} </script>
