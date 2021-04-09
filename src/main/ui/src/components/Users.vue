ce8656087d1191b7fc6b45f61a9d46202bfbaeff
venustuspl <venustus.pl@gmail.com>
2021-04-09 20:42:01 +0200

    fix: deleting user with correct id

--- src/main/ui/src/components/Users.vue
+++ src/main/ui/src/components/Users.vue
@@ -14,11 +14,11 @@
             </thead>
             <tbody>
               <tr v-for="item in users" :key="item.id">
-                  <td>{{ item.id }}</td>
+                  <td id="itemid" >{{ item.id }}</td>
                   <td>{{ item.firstName }}</td>
                   <td>{{ item.lastName }}</td>
                   <td>{{ item.email }}</td>
-                  <td> <button id="deletebutton" :value="item.id" type="button" @click='deleteUsers()' >Delete</button></td>
+                  <td><button id="deletebutton" type="button" @click='deleteUsers(item.id)'>Delete</button></td>
               </tr>
             </tbody>
         </table>
@@ -35,8 +35,8 @@ export default {
         props: ['users'],
          methods: {
 
-        deleteUsers() {
-        var userid = document.getElementById("deletebutton").value;
+        deleteUsers(userid) {
+       // var userid = document.getElementById("itemid").innerHTML;
         console.log(userid);
             deleteUser(userid).then(response => {
 console.log(response);
