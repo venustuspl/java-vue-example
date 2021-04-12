b4cdd156fd7eecea88fefe79d9225850844b3830
venustuspl <venustus.pl@gmail.com>
2021-04-12 20:33:06 +0200

    feat: added test shouldThrowErrorSavingUserWithExistsId

---
+++ src/test/java/com/venustus/users/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.venustus.users.service;
+
+import com.venustus.users.entity.User;
+import com.venustus.users.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UserServiceTest {
+
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private UserService userService;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(userRepository);
+        user = new User("test", "test", "test@test.pl");
+    }
+
+    @Test
+    void shouldThrowErrorSavingUserWithExistsId() {
+        //given
+        User testUser = new User("testUser", "testUser", "test@test.pl");
+        when(userRepository.findByEmail("test@test.pl")).thenReturn(java.util.Optional.ofNullable(user));
+        //when
+        Throwable throwable = Assertions.catchThrowable(() -> userService.saveUser(testUser));
+        //then
+        assertThat(throwable).hasMessage("This email address is already being used!");
+    }
+
+}
