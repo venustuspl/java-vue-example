6cfcff899b055e94673f74e61683e9e5992f2135
venustuspl <venustus.pl@gmail.com>
2021-04-13 20:15:59 +0200

    feat: added it test: shouldSaveUserInDb

---
+++ src/test/java/com/venustus/users/service/UserServiceTestIT.java
@@ -0,0 +1,43 @@
+package com.venustus.users.service;
+
+import com.venustus.users.entity.User;
+import com.venustus.users.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class UserServiceTestIT {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    @AfterEach
+    void clear() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void shouldSaveUserInDb() {
+        //given
+        User user = getUserToSave();
+        //when
+        User savedUser = userService.saveUser(user);
+        //then
+        assertEquals(user.getEmail(), savedUser.getEmail());
+        assertNotNull(savedUser.getId());
+    }
+
+    private User getUserToSave() {
+        return new User("testUser", "testUser", "test@test.pl");
+    }
+}
