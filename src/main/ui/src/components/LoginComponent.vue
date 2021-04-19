572eaf0b17bcb9917c62c20271838828526e8ef9
venustuspl <venustus.pl@gmail.com>
2021-04-19 19:10:40 +0200

    feat: added login component

---
+++ src/main/ui/src/components/LoginComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-group" :class="{ 'hasError': v.$error }">
+    <label class="mr-2 font-bold text-grey">Login</label>
+    <input type="text" class="input" v-model="login" placeholder="login" @input="v.$touch()">
+    <div class="text-sm mt-2 text-red" v-if="v.$error">
+      <div v-if="!v.required">Login is required</div>
+      <div v-if="!v.login">Login is not a properly formatted</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+
+    v: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    login: {
+      get() {
+        return this.value;
+      },
+
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
