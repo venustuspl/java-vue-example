ca5a8aa73825f738fb7f53548db095b5d17ecf39
venustuspl <venustus.pl@gmail.com>
2021-04-23 19:47:22 +0200

    refactor: founded Sonar problems

--- src/main/java/com/venustus/users/links/UserLinks.java
+++ src/main/java/com/venustus/users/links/UserLinks.java
@@ -8,4 +8,7 @@ public class UserLinks {
     public static final String ADD_USER = "/user";
     public static final String LIST_USERS_BY_EMAIL = "/users/email";
     public static final String DELETE_USER = "/users/{id}/delete";
+
+    private UserLinks() {
+    }
 }
