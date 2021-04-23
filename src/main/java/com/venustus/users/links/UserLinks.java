bdf287ca5d2ab3a4ec5c80140a1365fdb4739500
venustuspl <venustus.pl@gmail.com>
2021-04-23 19:43:44 +0200

    refactor: founded Sonar problems

--- src/main/java/com/venustus/users/links/UserLinks.java
+++ src/main/java/com/venustus/users/links/UserLinks.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserLinks {
-	public static final String LIST_USERS = "/users";
+    public static final String LIST_USERS = "/users";
     public static final String ADD_USER = "/user";
     public static final String LIST_USERS_BY_EMAIL = "/users/email";
     public static final String DELETE_USER = "/users/{id}/delete";
+
+    private UserLinks() {
+    }
 }
