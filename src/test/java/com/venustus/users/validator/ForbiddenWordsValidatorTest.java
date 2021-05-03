10dbe952ea9a7b615aac4470a140c6f96ec9d47a
venustuspl <venustus.pl@gmail.com>
2021-05-03 17:15:03 +0200

    feature: added ForbiddenWordsValidatorTest

---
+++ src/test/java/com/venustus/users/validator/ForbiddenWordsValidatorTest.java
@@ -0,0 +1,29 @@
+package com.venustus.users.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ForbiddenWordsValidatorTest {
+    private ForbiddenWordsValidator forbiddenWordsValidator;
+
+    @BeforeEach
+    public void init() {
+        forbiddenWordsValidator = new ForbiddenWordsValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello, shit happens, chuk, kurde."})
+    void shouldContainForbiddenWord(String input) {
+        // given
+        // when
+        List<String> list = forbiddenWordsValidator.validate(input);
+
+        // then
+        assertThat(list.size()).isEqualTo(1);
+    }
+}
