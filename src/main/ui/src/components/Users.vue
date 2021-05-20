d922c98df2042698de003d99a848e61e2099c2b8
venustuspl <venustus.pl@gmail.com>
2021-05-20 20:21:11 +0200

    feat: Users view.js vile

--- src/main/ui/src/components/Users.vue
+++ src/main/ui/src/components/Users.vue
@@ -15,16 +15,20 @@
             </tr>
             </thead>
             <tbody>
+
               <tr v-for="item in users" :key="item.id">
+
                   <td id="itemid" >{{ item.id }}</td>
                   <td><input type="text" class="input" v-model="firstName" :placeholder=item.firstName @input="v.$touch()"></td>
                   <td><input type="text" class="input" v-model="lastName" :placeholder=item.lastName @input="v.$touch()"></td>
                   <td><input type="text" class="input" v-model="login" :placeholder=item.login @input="v.$touch()"></td>
                   <td><input type="text" class="input" v-model="email" :placeholder=item.email @input="v.$touch()"> </td>
                   <td><button id="deletebutton" type="button" @click='deleteUsers(item.id)'>Delete</button>
-                  <button id="updatebutton" type="button" @click='updateUsers(item.id)'>Update</button>
+                  <button id="updatebutton" type="button" @click='updateUsers()'>Update</button>
                   </td>
+
               </tr>
+
             </tbody>
         </table>
     </div>
@@ -36,13 +40,76 @@ import {
     deleteUser
 } from '../services/UserService'
 
+
+
 export default {
         name: 'Users',
-        props: ['users'],
+
+         props: {
+                 value: {
+                       type: String,
+                       default: ""
+                     },
+
+                     v: {
+                       type: Object,
+                       required: true
+                     },
+         users:{}
+          },
+          computed: {
+          firstName: {
+                          get() {
+                            return this.value;
+                          },
+
+                          set(value) {
+                            this.$emit("input", value);
+                          }
+                        },
+          lastName: {
+                          get() {
+                            return this.value;
+                          },
+
+                          set(value) {
+                            this.$emit("input", value);
+                          }
+                        },
+              login: {
+                get() {
+                  return this.value;
+                },
+
+                set(value) {
+                  this.$emit("input", value);
+                }
+              },
+              email: {
+                              get() {
+                                return this.value;
+                              },
+
+                              set(value) {
+                                this.$emit("input", value);
+                              }
+
+            }
+            },
          methods: {
-updateUsers(userid) {
-        console.log(userid);
-            updateUser(userid).then(response => {
+updateUsers() {
+          this.$v.$touch();
+            // if its still pending or an error is returned do not submit
+          if (this.$v.$pending || this.$v.$error) return;
+          console.log(this.firstName)
+          const payload = {
+              firstName: this.firstName,
+              lastName: this.lastName,
+              login: this.login,
+              email: this.email
+          }
+        console.log(payload);
+            updateUser(payload).then(response => {
 console.log(response);
 this.$emit('getAllUsers');
 this.$forceUpdate();
